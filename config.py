0fbb286a247f8d67548b91a17710f3a99f36ec47
manavok <mitmanav6805@gmail.com>
2025-05-29 20:37:42 +0530

    initial configuration

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+dir = os.path.dirname(__file__)
+basedir = os.path.abspath(dir)
+
+class Config:
+    # === Flask Security ===
+    SECRET_KEY=os.getenv("SECRET_KEY")
+    # === Database ===
+    SQLALCHEMY_DATABASE_URI=os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS=False = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS", 'False') == 'True'
+    # === Mail Configuration ===
+
+    MAIL_SERVER = os.getenv("MAIL_SERVER")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", 'True') == 'True'
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DEV_DATABASE_URL') or 'sqlite:///' + 'sqlite://'
+
+class ProductionConfig(Config):
+    pass
+
+Config = {
+    'development' : DevelopmentConfig,
+    'testing' : TestingConfig,
+    'production' : ProductionConfig,
+
+    'default' : DevelopmentConfig
+}
+
+ 
